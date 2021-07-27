f4263739bbdcf2c690dbf6ce2b94e508c4e7f565
Rafael Winterhalter <rafael.wth@gmail.com>
2021-07-27 23:19:24 +0200

    Add additional license header and javadoc.

--- code/src/main/java/com/googlecode/concurrenttrees/radix/node/SimpleNodeList.java
+++ code/src/main/java/com/googlecode/concurrenttrees/radix/node/SimpleNodeList.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2013 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.concurrenttrees.radix.node;
 
 import java.util.Arrays;
@@ -5,6 +20,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A simple implementation of a {@link NodeList} backed by a {@link List}.
+ */
 public class SimpleNodeList implements NodeList {
 
     private final List<Node> nodes;
