11a3677f22ac7fbbb21f2397ea5629e25f78195b
Rafael Winterhalter <rafael.wth@gmail.com>
2021-07-27 23:17:58 +0200

    Add missing javadoc and header.

--- code/src/main/java/com/googlecode/concurrenttrees/radix/node/NodeList.java
+++ code/src/main/java/com/googlecode/concurrenttrees/radix/node/NodeList.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2012-2013 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.concurrenttrees.radix.node;
 
 import java.util.Collection;
 
+/**
+ * A list of {@link Node}s represented by a tree. This interface is used rather then a Java list, 
+ * to allow representing concurrent arrays as such lists and to avoid allocating wrapper instances.
+ */
 public interface NodeList {
-
+    
     int size();
 
     Node get(int index);
